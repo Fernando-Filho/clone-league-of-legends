64db7669f6a008285bf0b438b83dddb4162dbf0d
Mirella Naspolini <104786840+mirellanaspolini@users.noreply.github.com>
2024-05-07 09:54:04 -0300

    merge  (#10)
    * Branch mi (#6)
    
    * API: reestabelecendo conexao com a api
    
    * README: adicionando informações
    
    * API: corrigindo erro de ramificação
    
    * Ajustado montagem da url (#7)
    
    * Feat url api (#8)
    
    * Ajustado montagem da url
    
    * ajuste montagem url
    
    * Feat url api (#9)
    
    * Update index.html
    
    * Update script.js
    
    ---------
    
    Co-authored-by: Vinícius Porto <81120004+ViniPorto@users.noreply.github.com>

--- client/loja/campeoes/script.js
+++ client/loja/campeoes/script.js
@@ -1,52 +1,90 @@
-function constroiURL(){
+var tokenJWT = "";
+
+async function carregarTokenEChamps() {
+    let url = "http://localhost:8080/login";
+    let dados = {
+        login: "admin",
+        senha: "admin",
+    };
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dados),
+    });
+
+    const json = await response.json();
+
+    tokenJWT = await json.token;
+
+    await carregarChamps();
+}
+
+function constroiURL() {
     let url = "http://localhost:8080/champ?size=200";
     let name = document.getElementById("input-search-champ").value;
-    let filters = [];
-    for(let i = 0; i < 8; i++){
-        if(document.querySelectorAll(".input-checkbox")[i].checked){
-            filters.push(document.querySelectorAll(".input-checkbox")[i].id);
+    let filtersRole = [];
+    for (let i = 0; i < 6; i++) {
+        if (document.querySelectorAll(".input-checkbox")[i].checked) {
+            filtersRole.push(
+                document.querySelectorAll(".input-checkbox")[i].id
+            );
         }
     }
-    let sortingMethod = document.getElementById("select-sorting-method").options[document.getElementById("select-sorting-method").selectedIndex].value; 
-    if(name.length == 0 && filters.length == 0){
-        if(sortingMethod == 2){
-            return url;
-        }
+    let sale = document.getElementById("sale");
+    let sortingMethod = document.getElementById("select-sorting-method")
+        .options[document.getElementById("select-sorting-method").selectedIndex]
+        .value;
+    if (name.length == 0 && filtersRole.length == 0 && !sale.checked) {
+        url += `&sale=false&sort=${sortingMethod}`;
+        console.log(url);
+        return url;
     }
-    url += "&"
-    if(name.length != 0){
+    url += "&";
+    if (name.length != 0) {
         url += `name=${name}`;
     }
-    if(filters.length != 0){
-        if(name.length != 0){
+    if (filtersRole.length != 0) {
+        if (name.length != 0) {
             url += "&";
         }
         let adicionados = 0;
-        for(let i = 0; i < filters.length; i++){
-            if(adicionados != 0){
-                url += "&";
+        for (let i = 0; i < filtersRole.length; i++) {
+            if (adicionados != 0) {
+                url += ",";
+            } else {
+                url += `roles=`;
             }
-            url += `filters=${filters[i]}`;
+            url += filtersRole[i];
             adicionados++;
         }
     }
-    if(name.length != 0 || filters.length != 0){
+    if (name.length != 0 || filtersRole.length != 0) {
         url += "&";
     }
-    url += `sortingMethod=${parseInt(sortingMethod)}`;
+    url += `sale=${sale.checked}`;
+    url += `&sort=${sortingMethod}`;
     console.log(url);
     return url;
 }
 
-async function carregarChamps(){
-    const dados = await fetch(constroiURL());
+async function carregarChamps() {
+    const dados = await fetch(constroiURL(), {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${tokenJWT}`,
+        },
+    });
+
     const json = await dados.json();
     const data = await json.content;
 
     const divChamps = document.getElementById("grid-campeoes");
     divChamps.style.overflowY = "scroll";
     divChamps.innerHTML = "";
-    data.forEach(champ => {
+    data.forEach((champ) => {
         divChamps.innerHTML += `<div class="card">
                                     <img src="${champ.image}" width="240" height="240" alt="${champ.name}">
                                     <div class="card-info-bg">
@@ -66,12 +104,23 @@ for(let i = 0; i < elementsTriggerEventCheckbox.length; i++){
     elementsTriggerEventCheckbox[i].addEventListener('click', carregarChamps);
 }
 
-let elementTriggerEventSelect = document.getElementById("select-sorting-method");
+let elementsTriggerEventCheckbox =
+    document.getElementsByClassName("input-checkbox");
 
-elementTriggerEventSelect.addEventListener('change', carregarChamps);
+for (let i = 0; i < elementsTriggerEventCheckbox.length; i++) {
+    elementsTriggerEventCheckbox[i].addEventListener("click", carregarChamps);
+}
+
+document.getElementById("sale").addEventListener("click", carregarChamps);
+
+let elementTriggerEventSelect = document.getElementById(
+    "select-sorting-method"
+);
+
+elementTriggerEventSelect.addEventListener("change", carregarChamps);
 
 let elementTriggerEventSearch = document.getElementById("input-search-champ");
 
-elementTriggerEventSearch.addEventListener('input', carregarChamps);
+elementTriggerEventSearch.addEventListener("input", carregarChamps);
 
-window.addEventListener("load", carregarChamps());
+window.addEventListener("load", carregarTokenEChamps());
